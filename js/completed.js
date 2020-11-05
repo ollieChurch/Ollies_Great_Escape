67298855f14c195d2dbc8208d0d7c3122b467dc4
Squeak-Paradox <69602381+Squeak-Paradox@users.noreply.github.com>
2020-11-05 13:06:58 +0000

    initial upload

---
+++ js/completed.js
@@ -0,0 +1,476 @@
+let totalRoomCount = 0;
+let totalSuccessCount = 0;
+let totalFailCount = 0;
+
+// !! ROOM LIST !!
+const roomData = [
+    {
+        company: "Mystery Rooms UK",
+        location: "Camberley, UK",
+        coordinates: "GU15 3PQ",
+        success: [
+            "The Suspect"
+        ],
+        fail: []
+    },
+    {
+        company: "Cryptic Escapes",
+        location: "Norwich, UK",
+        coordinates: "NR3 1HF",
+        success: [],
+        fail: [
+            "The Treasure Of Green Beard",
+            "The Haunted"
+        ]
+    },
+    {
+        company: "Paradox Parlours",
+        location: "Guildford, UK",
+        coordinates: "GU1 4AP",
+        success: [
+            "The Case Of The Missing Gun",
+            "Venus"
+        ],
+        fail: []
+    },
+    {
+        company: "Paradox Parlours",
+        location: "Dorking, UK",
+        coordinates: "RH5 6AA",
+        success: [
+            "Missing In Action"
+        ],
+        fail: []
+    },
+    {
+        company: "Deadlocked",
+        location: "Reading, UK",
+        coordinates: "RG1 7RG",
+        success: [
+            "The Phoenix Research",
+            "The Testing Chamber"
+        ],
+        fail: []
+    },
+    {
+        company: "Escape Reading",
+        location: "Reading, UK",
+        coordinates: "RG1 7QF",
+        success: [
+            "The Library",
+            "The Bank Robbery",
+            "Vampire",
+            "Kidnapped",
+            "Blown Away",
+            "The Bank Robbery (new)"
+        ],
+        fail: []
+    },
+    {
+        company: "Knockout",
+        location: "Reading, UK",
+        coordinates: "RG1 8LG",
+        success: [
+            "9 to 5 Assasin",
+            "Prison Break",
+            "Mad Scientist"
+        ],
+        fail: []
+    },
+    {
+        company: "Time Trap",
+        location: "Reading, UK",
+        coordinates: "RG1 1DB",
+        success: [
+            "Rebellion",
+            "Imaginarium",
+            "The Divide",
+            "Station X"
+        ],
+        fail: []
+    },
+    {
+        company: "Containment",
+        location: "Guildford, UK",
+        coordinates: "GU3 3PE",
+        success: [
+            "The Vault",
+            "The Bomb",
+        ],
+        fail: []
+    },
+    {
+        company: "Exciting Escapes",
+        location: "Basingstoke, UK",
+        coordinates: "RG21 7NE",
+        success: [
+            "Murder At The Movies",
+            "Dark Deeds and Groceries",
+        ],
+        fail: []
+    },
+    {
+        company: "Tulley's Farm",
+        location: "Crawley, UK",
+        coordinates: "RH10 4PE",
+        success: [
+            "Mutiny",
+            "Outfitters",
+            "Dodge"
+        ],
+        fail: [
+            "Nethercott Manor"
+        ]
+    },
+    {
+        company: "Escape Rooms",
+        location: "London, UK",
+        coordinates: "SE1 2TU",
+        success: [],
+        fail: [
+            "Pharaoh's Chamber"
+        ]
+    },
+    {
+        company: "Clue Quest",
+        location: "London, UK",
+        coordinates: "N1 0SL",
+        success: [
+            "Operation Blacksheep",
+            "Revenge Of The Sheep"
+        ],
+        fail: []
+    },
+    {
+        company: "Pier Pressure",
+        location: "Brighton, UK",
+        coordinates: "BN1 3FG",
+        success: [
+            "Modrophenia",
+            "Raver Quest"
+        ],
+        fail: []
+    },
+    {
+        company: "Bewilderbox",
+        location: "Brighton, UK",
+        coordinates: "BN1 4GU",
+        success: [
+            "The Bewilderbox Initiavtive",
+            "Judgement D.A.V.E."
+        ],
+        fail: []
+    },
+    {
+        company: "Handmade Mysteries",
+        location: "Brighton, UK",
+        coordinates: "BN1 4JE",
+        success: [
+            "Lady Chastity's Reserve"
+        ],
+        fail: []
+    },
+    {
+        company: "ExCLUEsive Games",
+        location: "Newbury, UK",
+        coordinates: "BN1 4JE",
+        success: [
+            "Lost Island Escape",
+            "Wild Wild West"
+        ],
+        fail: []
+    },
+    {
+        company: "The Escape Game",
+        location: "Orlando, FL, USA",
+        coordinates: "32819",
+        success: [
+            "Special Ops: Mysterious Market",
+            "Prison Break"
+        ],
+        fail: []
+    },
+    {
+        company: "Aroom",
+        location: "Budapest, Hungary",
+        coordinates: "1061",
+        success: [
+            "The Metro",
+            "The Heist"
+        ],
+        fail: []
+    },
+    {
+        company: "Mars Mission",
+        location: "Budapest, Hungary",
+        coordinates: "1014",
+        success: [
+            "Mars Mission"
+        ],
+        fail: []
+    },
+    {
+        company: "Mystique Room",
+        location: "Budapest, Hungary",
+        coordinates: "1055",
+        success: [
+            "Pirate Bay",
+            "The Tower Of Wollongong",
+            "Time Machine",
+            "The Irone Throne",
+            "The Pharaoh's Burial Chamber",
+            "Shogun's Shadow",
+            "The Cathedral"
+        ],
+        fail: []
+    },
+    {
+        company: "Neverland",
+        location: "Budapest, Hungary",
+        coordinates: "1074",
+        success: [
+            "Prison Break"
+        ],
+        fail: []
+    },
+    {
+        company: "Goszdu Mission",
+        location: "Budapest, Hungary",
+        coordinates: "1072",
+        success: [
+            "The White Room",
+            "Prison Mission",
+            "Mafia Mission"
+        ],
+        fail: []
+    },
+    {
+        company: "E-Exit",
+        location: "Budapest, Hungary",
+        coordinates: "1072",
+        success: [
+            "Secret Subway",
+            "1984",
+            "Heaven And Hell",
+            "Santa Muerte"
+        ],
+        fail: []
+    },
+    {
+        company: "Locked Room",
+        location: "Budapest, Hungary",
+        coordinates: "1061",
+        success: [
+            "Serial Killer",
+            "Research Base",
+        ],
+        fail: []
+    },
+    {
+        company: "Clue Cracker",
+        location: "Tunbridge Wells, UK",
+        coordinates: "TN4 9PE",
+        success: [
+            "Jail Break",
+        ],
+        fail: []
+    },
+    {
+        company: "Clued Up",
+        location: "Norwich, UK",
+        coordinates: "NR1 1LT",
+        success: [
+            "Nuclear Countdown",
+        ],
+        fail: []
+    },
+    {
+        company: "Escapement",
+        location: "Margate, UK",
+        coordinates: "CT9 2RJ",
+        success: [
+            "Pirates Of Polaris",
+            "The Pit",
+            "Egyptian Exodus"
+        ],
+        fail: []
+    },
+    {
+        company: "Panic Rooms",
+        location: "Gravesend, UK",
+        coordinates: "DA12 2AH",
+        success: [
+            "Dino Land",
+            "Loop",
+        ],
+        fail: []
+    },
+    {
+        company: "Mind Maze",
+        location: "Prague, Czech Republic",
+        coordinates: "Prague 2",
+        success: [
+            "Galactic Pioneers",
+        ],
+        fail: []
+    },
+    {
+        company: "The Chamber",
+        location: "Prague, Czech Republic",
+        coordinates: "Prague 5",
+        success: [
+            "Wonderland",
+        ],
+        fail: []
+    },
+    {
+        company: "A.I. Escapes",
+        location: "London, UK",
+        coordinates: "SE16 4DG",
+        success: [
+            "M.A.D.",
+            "Project Delta"
+        ],
+        fail: []
+    },
+    {
+        company: "Houdini's",
+        location: "Southampton, UK",
+        coordinates: "SO14 0JD",
+        success: [
+            "Titanic",
+            "Escape From Alcatraz",
+            "Extinct"
+        ],
+        fail: []
+    },
+]
+const roomsContainer = document.getElementById("rooms")
+
+for (let i = 0; i < roomData.length; i++) {
+    const newCompany = document.createElement("div")
+    const companyName = document.createElement("h3")
+    const companyLocationContainer = document.createElement("p")
+    const companyLocation = document.createElement("em")
+    const companyRooms = document.createElement("ul")
+    
+    newCompany.classList.add("company-container")
+    
+    companyName.textContent = roomData[i].company
+    companyName.classList.add("company-name")
+    
+    companyLocation.textContent = roomData[i].location
+    companyLocationContainer.append(companyLocation)
+    companyLocationContainer.classList.add("company-location")
+    
+    companyRooms.classList.add("company-room-list")
+    
+    roomData[i].success.forEach(name => {
+        const newRoom = document.createElement("li")
+        newRoom.textContent = name
+        newRoom.classList.add("company-room")
+        companyRooms.append(newRoom)
+        totalRoomCount ++
+        totalSuccessCount ++
+    })
+    
+    roomData[i].fail.forEach(name => {
+        const newRoom = document.createElement("li")
+        newRoom.textContent = name + " [FAIL]"
+        newRoom.classList.add("company-room")
+        newRoom.classList.add("company-room-fail")
+        companyRooms.append(newRoom)
+        totalRoomCount ++
+        totalFailCount ++
+    })
+    
+    newCompany.append(companyName)
+    newCompany.append(companyLocationContainer)
+    newCompany.append(companyRooms)
+
+    roomsContainer.append(newCompany)
+}
+
+// !! ROOM COUNT !!
+let count = 0
+let successRoomCount = 0
+let failRoomCount = 0
+
+const countTimer = setInterval(countUp, 40)
+const failCountTimer = setInterval(failCountUp, 500)
+
+function countUp() {
+    const numberDisplay = document.getElementById("room-count-number")
+    
+    if (count < totalRoomCount) {
+        if (count < totalSuccessCount) {
+            successRoomCount ++
+            document.getElementById("success-count").textContent = successRoomCount + " succesful escapes"
+        }
+
+        count ++
+        numberDisplay.textContent = count
+    } else {
+        clearInterval(countTimer)
+        numberDisplay.classList.add("count-finished")
+    }
+}
+
+function failCountUp() {
+    if (failRoomCount < totalFailCount) {
+        failRoomCount ++
+        document.getElementById("fail-count").textContent = failRoomCount + " failed escapes"
+    } else {
+        clearInterval(failCountUp)
+    }
+}
+
+// !! ROOM SEARCH !!
+const roomSearch = document.getElementById("room-search")
+const searchInput = document.getElementById("room-search-input")
+const companies = document.getElementsByClassName("company-container")
+
+roomSearch.addEventListener("submit", function(event) {
+    event.preventDefault()
+})
+
+searchInput.addEventListener("keyup", function() {
+    const searchValue = searchInput.value.toLowerCase()
+    
+    for (let i = 0; i < companies.length; i++) {
+        const company = roomData[i]
+        const name = company.company.toLowerCase()
+        const location = company.location.toLowerCase()
+        
+        let rooms = []
+        company.success.forEach(room => {
+            const newRoom = room.toLowerCase()
+            rooms.push(newRoom)
+        })
+        company.fail.forEach(room => {
+            const newRoom = room.toLowerCase()
+            rooms.push(newRoom)
+        })
+        
+        let roomMatch = false
+        rooms.forEach(room => {
+            if (room.includes(searchValue)) {
+                roomMatch = true
+            }
+        })
+                
+        if (name.includes(searchValue) || location.includes(searchValue) || roomMatch) {
+            companies[i].style.display = "block"
+        } else {
+            companies[i].style.display = "none"
+        }
+    }
+})
+
+
+
+
+
+
+
+
---
+++ js/top.js
@@ -0,0 +1,107 @@
+const topData = [
+    {
+        room: "Secret Subway",
+        company: "E-Exit",
+        location: "Budapest, Hungary",
+        description: "A MOVING TRAIN CARRIAGE! Not that anything else really needs to be said but this room was full of surprises and a great flow, everything made sense and there was a palpable excitement from everyone on the team. Brilliant concept of having to take our power generator with us between rooms as well."
+    },
+    {
+        room: "Galactic Pioneers",
+        company: "Mind Maze",
+        location: "Prague, Czech Republic",
+        description: "Played as a two and it was a whole lot of fun. Puzzles required some thinking and working out but nothing extensively difficult and we managed to smash it without worrying too much about time. The whole experience was extremely story led and immersive in design and finished with a Space Team-esque puzzle that was fun and frantic. Good, characterful clue system as well."
+    },
+    {
+        room: "Special Ops: Mysterious Market",
+        company: "The Escape Game",
+        location: "Orlando, FL, USA",
+        description: "This was a beautifully decorated room with high tech puzzles, immersive atmosphere, and several twist endings. Let down by two major tech malfunctions but still hugely impressive and co-operative puzzling. This really demonstrated what a big budget has the potential to create."
+    },
+    {
+        room: "The Case Of The Missing Gun",
+        company: "Paradox Parlours",
+        location: "Guildford, UK",
+        description: "My third escape room, highly immersive and story driven. Puzzle structure played to my strengths and loved the secret room and compartment surprises. Mad and energetic excitement and fun throughout and excellent hosting."
+    },
+    {
+        room: "Rebellion",
+        company: "Time Trap",
+        location: "Reading, UK",
+        description: "Creating a castle inside some fairly standard modern day rooms is no easy task but Time Trap smashed it. I loved all of the physical analogue mechanics that made this room work and the non-linear puzzles which ended up linking together."
+    },
+    {
+        room: "Venus",
+        company: "Paradox Parlours",
+        location: "Guildford, UK",
+        description: "Even as the first team to test this room the potential behind it was huge. Excellent puzzles which go deeper and require more careful thought than most. But the standout is Venus herself, a fully interactive and hilarious A.I. I could have spent the whole time just chatting to her."
+    },
+    {
+        room: "Pirate Bay",
+        company: "Mystique Room",
+        location: "Budapest, Hungary",
+        description: "This has to be the most impressive set I have ever walked into. Looking up at a full size pirate ship while stood on the dock, climbing up onto the deck, down into the ship, up on top of the ship. An experience not rivalled by any other room."
+    },
+    {
+        room: "The Bank Robbery (new)",
+        company: "Escape Reading",
+        location: "Reading, UK",
+        description: "Bursting out of the back of a van into a convincingly modern day bank, cracking a giant classic looking vault door as well as ripping an ATM open by chaining it to the van. This room was a lot of fun in so many places."
+    },
+    {
+        room: "Jail Break",
+        company: "Clue Cracker",
+        location: "Tunbridge Wells, UK",
+        description: "A room with heavy attention to detail. Small on size but big on heart. One puzzle with a leap of logic and an interesting clue system that does not have the flexibility often required but intends to keep the immersion of the story. A classic room very well executed and hosted well."
+    },
+    {
+        room: "Judgement Dave",
+        company: "Bewilderbox",
+        location: "Brighton, UK",
+        description: "Small space, beautifully immersive from ceiling to floor. Strong story and interesting moral choice at the end. Added interactive A.I./clue system an excellent concept but not quite executed to match (Venus is better). The room felt like a whole other world from the pub it was based in."
+    },
+]
+const chartsList = document.getElementById("top-charts")
+
+for (let i = 0; i < topData.length; i++) {
+    console.log("running for loop")
+    const newEntry = document.createElement("div")
+    const chartNumberContainer = document.createElement("div")
+    const chartNumber = document.createElement("h3")
+    const entryText = document.createElement("div")
+    const roomName = document.createElement("h3")
+    const roomDetailsContainer = document.createElement("p")
+    const roomDetails = document.createElement("em")
+    const roomDescription = document.createElement("p")
+    
+    roomDescription.textContent = topData[i].description
+    
+    roomDetails.textContent = topData[i].company + " | " + topData[i].location
+    roomDetailsContainer.append(roomDetails)
+    roomDetailsContainer.classList.add("top-details")
+    
+    roomName.textContent = topData[i].room
+    roomName.classList.add("top-name")
+    
+    entryText.append(roomName)
+    entryText.append(roomDetailsContainer)
+    entryText.append(roomDescription)
+    entryText.classList.add("room-container-text")
+    
+    const number = i + 1
+    chartNumber.textContent = number + "."
+    chartNumberContainer.append(chartNumber)
+    chartNumberContainer.classList.add("room-container-number")
+    
+    newEntry.append(chartNumberContainer)
+    newEntry.append(entryText)
+    newEntry.classList.add("room-container")
+    
+    if (topData.length % 2 === 0) {
+        if (i === 0 || i === (topData.length - 1)) {
+            newEntry.classList.add("room-container-wide")
+        }
+    }
+    
+    chartsList.append(newEntry)
+    console.log(newEntry)
+}
